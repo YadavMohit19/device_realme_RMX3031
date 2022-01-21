b2c309566a13d8efb99a7228cdd9e9da8357a12a
yadavmohit19 <mohityadav050575@gmail.com>
2022-01-21 08:01:11 +0000

    1

--- device.mk
+++ device.mk
@@ -118,23 +118,6 @@ PRODUCT_SOONG_NAMESPACES += \
 -include $(DEVICE_PATH)/system_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
-# Symbols
-PRODUCT_PACKAGES += \
-    libshim_vtservice
-
-# Telephony
-PRODUCT_BOOT_JARS += \
-    mediatek-common \
-    mediatek-framework \
-    mediatek-ims-base \
-    mediatek-ims-common \
-    mediatek-telecom-common \
-    mediatek-telephony-base \
-    mediatek-telephony-common
-
-PRODUCT_PACKAGES += \
-    ImsServiceBase
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     TetheringConfigOverlay \
